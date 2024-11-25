9ed9ed4dc5a982d50e46c55a182e390d06ebfe3c
Алексей <leha2017tv@gmail.com>
2024-11-25 10:27:06 +0500

    feat: add form

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,50 @@
 </script>
 
 <template>
-<button type="button" class="btn btn-warning">Warning</button>
+
+<div class="container">
+<div class="row">
+  <div class="col">
+  
+      <h1 class="text-center ma-3">учет товаров</h1>
+      <div class="mb-3">
+      <label for="name" class="form-label">название</label>
+      <input type="text" class="form-control" id="name" >
+     </div>
+
+
+     <div class="mb-3">
+      <label for="date" class="form-label">Дата дабовления </label>
+  <input type="date" class="form-control" id="date" >
+     </div>
+
+
+
+     <div class="mb-3">
+      <label for="count" class="form-label">количество</label>
+  <input type="numder" class="form-control" id="count" >
+     </div>
+
+     
+
+     <div class="mb-3">
+      <label for="price" class="form-label">цена</label>
+  <input type="namber" class="form-control" id="price" >
+     </div>
+
+
+
+     <button type="button" class="btn btn-dark">добавить</button>
+
+
+
+     
+
+</div>
+</div>
+</div>
+
+
+
 </template>
 
